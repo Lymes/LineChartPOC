46d3de027d2b0d7ec912bba98a5f9d542f40c8d5
Leonid Mesentsev <l.mesentsev@gmail.com>
2023-02-05 19:56:52 +0100

    refactoring

---
+++ LineChartPOC/App/UI/AZLineChartView/AZLineChartView+Config.swift
@@ -0,0 +1,70 @@
+//
+//  AZLineChartView+Config.swift
+//  LineChartPOC
+//
+//  Created by Leonid Mesentsev on 05/02/23.
+//
+
+import UIKit
+import Charts
+
+extension AZLineChartView {
+
+    func setupStyle() {
+        setupRenderer()
+        setupBehavior()
+        setupXAxis()
+    }
+
+    func decorateDataSet(_ dataSet: LineChartDataSet, _ index: Int) -> LineChartDataSet {
+        dataSet.lineWidth = 1
+        dataSet.colors = [style.dataColors[index]]
+        dataSet.drawCircleHoleEnabled = false
+        dataSet.drawCirclesEnabled = false
+        dataSet.drawValuesEnabled = true
+        dataSet.highlightLineWidth = 2
+        dataSet.highlightColor = .red
+        dataSet.drawHorizontalHighlightIndicatorEnabled = false
+        return dataSet
+    }
+
+    private func setupBehavior() {
+        dragEnabled = true
+        setScaleEnabled(false)
+        pinchZoomEnabled = false
+        doubleTapToZoomEnabled = false
+        highlightPerTapEnabled = false
+        dragEnabled = false
+        marker = AZLineChartMarker()
+        setupGestures()
+    }
+
+    private func setupXAxis() {
+        xAxis.labelFont = style.xAxisLabelFont
+        xAxis.labelTextColor = style.xAxisLabelTextColor
+        xAxis.drawAxisLineEnabled = false
+        xAxis.labelCount = style.xAxisLabelCount
+        xAxis.labelPosition = .bottom
+        xAxis.drawLabelsEnabled = true
+        xAxis.drawLimitLinesBehindDataEnabled = true
+        xAxis.avoidFirstLastClippingEnabled = true
+        xAxis.valueFormatter = AZLineChartXFormatter()
+        leftAxis.labelTextColor = style.leftAxisLabelTextColor
+        leftAxis.drawGridLinesEnabled = true
+        leftAxis.granularityEnabled = true
+        rightAxis.labelTextColor = style.rightAxisLabelTextColor
+        rightAxis.granularityEnabled = false
+    }
+
+    private func setupRenderer() {
+        if let defaultRenderer = renderer {
+            let animator = defaultRenderer.animator
+            let viewPort = defaultRenderer.viewPortHandler
+            renderer = AZLineChartRenderer(
+                dataProvider: self,
+                animator: animator,
+                viewPortHandler: viewPort
+            )
+        }
+    }
+}
--- LineChartPOC/App/UI/AZLineChartView/AZLineChartView.swift
+++ LineChartPOC/App/UI/AZLineChartView/AZLineChartView.swift
@@ -18,11 +18,14 @@ final class AZLineChartView: LineChartView, ChartViewDelegate {
 
     // MARK: Public API
 
-    var style: AZLineChartStyle = AZLineChartGenericStyle()
+    var style: AZLineChartStyle = AZLineChartGenericStyle() {
+        didSet {
+            setupStyle()
+        }
+    }
 
     var datasource: AZLineChartDataSource? {
         didSet {
-            setupStyle()
             datasource?.dataSetDecorator = decorateDataSet
             setupObservers()
         }
@@ -50,69 +53,11 @@ final class AZLineChartView: LineChartView, ChartViewDelegate {
 
     // MARK: Private methods
 
-    private func setupStyle() {
-        setupRenderer()
-        setupBehavior()
-        setupXAxis()
-    }
-
-    private func setupBehavior() {
-        dragEnabled = true
-        setScaleEnabled(false)
-        pinchZoomEnabled = false
-        doubleTapToZoomEnabled = false
-        highlightPerTapEnabled = false
-        dragEnabled = false
-        marker = AZLineChartMarker()
-        setupGestures()
-    }
-
-    private func setupXAxis() {
-        xAxis.labelFont = style.xAxisLabelFont
-        xAxis.labelTextColor = style.xAxisLabelTextColor
-        xAxis.drawAxisLineEnabled = false
-        xAxis.labelCount = style.xAxisLabelCount
-        xAxis.labelPosition = .bottom
-        xAxis.drawLabelsEnabled = true
-        xAxis.drawLimitLinesBehindDataEnabled = true
-        xAxis.avoidFirstLastClippingEnabled = true
-        xAxis.valueFormatter = AZLineChartXFormatter()
-        leftAxis.labelTextColor = style.leftAxisLabelTextColor
-        leftAxis.drawGridLinesEnabled = true
-        leftAxis.granularityEnabled = true
-        rightAxis.labelTextColor = style.rightAxisLabelTextColor
-        rightAxis.granularityEnabled = false
-    }
-
     private func setupObservers() {
         cancellable = datasource?.$chartData
             .receive(on: DispatchQueue.main)
             .sink { [weak self] data in
-            self?.data = data
-        }
-    }
-
-    private func setupRenderer() {
-        if let defaultRenderer = renderer {
-            let animator = defaultRenderer.animator
-            let viewPort = defaultRenderer.viewPortHandler
-            renderer = AZLineChartRenderer(
-                dataProvider: self,
-                animator: animator,
-                viewPortHandler: viewPort
-            )
-        }
-    }
-
-    private func decorateDataSet(_ dataSet: LineChartDataSet, _ index: Int) -> LineChartDataSet {
-        dataSet.lineWidth = 1
-        dataSet.colors = [style.dataColors[index]]
-        dataSet.drawCircleHoleEnabled = false
-        dataSet.drawCirclesEnabled = false
-        dataSet.drawValuesEnabled = true
-        dataSet.highlightLineWidth = 2
-        dataSet.highlightColor = .red
-        dataSet.drawHorizontalHighlightIndicatorEnabled = false
-        return dataSet
+                self?.data = data
+            }
     }
 }
